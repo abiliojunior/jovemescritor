d4465bd949c4b1cd39c5d4e2909b5485aa359eba
Abílio Junior <abiliojunior.mat@gmail.com>
2017-11-21 02:18:24 +0000

    So brincando

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html: "Ola Mundo"
+      render html: "VK é uma gatinha com mel"
   end
     
 end
