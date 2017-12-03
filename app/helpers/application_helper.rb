5ab8f5bfad3f5f88835a9ea945f1cba54ac6666d
Abílio Junior <abiliojunior.mat@gmail.com>
2017-12-03 17:46:20 +0000

    finalizado configuracao do bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     
     # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Jovem Escrito"
+    base_title = "Jovem Escritor"
     if page_title.empty?
       base_title
     else
